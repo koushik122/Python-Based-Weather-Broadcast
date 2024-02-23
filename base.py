4402325f5ce7923c30e6b01d2b123361b94c15d1
koushik122 <koushikpaul378@gmail.com>
2024-02-23 21:25:53 +0530

    Added Search bar and update resizability

--- base.py
+++ base.py
@@ -1,10 +1,25 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 root=Tk()
-root.geometry("555x400")
+
+# Geometry(WxH)
+root.geometry("760x450")
 root.title("Weather App")
-b=Label(text="apply")
-b.pack(anchor=W)
+
+# Changed resizability
+root.resizable(width=False,height=False)
+# Background colour update
+root.config(bg="#47ceff")
+
+
+# Search Bar
+image=Image.open("App icon resources/Search Bar.png")
+# (W,H)
+resized_image=image.resize((300,130))
+im=ImageTk.PhotoImage(resized_image)
+Label(image=im,bg="#47ceff").place(x=235,y=34)
+
 
 
 
