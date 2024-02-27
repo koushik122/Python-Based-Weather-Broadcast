2a8af1c4dc58ecbe367ceec636529d6bf0716cd5
koushik122 <koushikpaul378@gmail.com>
2024-02-27 12:21:30 +0530

    Added search button

--- base.py
+++ base.py
@@ -1,24 +1,38 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
-root=Tk()
 
-# Geometry(WxH)
+def search():
+    print("Search button clicked!")
+
+
+root = Tk()
+
+# Geometry (Width x Height)
 root.geometry("760x450")
 root.title("Weather App")
 
-# Changed resizability
-root.resizable(width=False,height=False)
-# Background colour update
+# Disallow resizing
+root.resizable(width=False, height=False)
+
+# Background colour
 root.config(bg="#47ceff")
 
 
-# Search Bar
-image=Image.open("App icon resources/Search Bar.png")
-# (W,H)
-resized_image=image.resize((300,130))
-im=ImageTk.PhotoImage(resized_image)
-Label(image=im,bg="#47ceff").place(x=235,y=34)
+# Search Bar image
+search_bar_image = Image.open("App icon resources/Search Bar.png")
+# (Width x Height)
+resized_search_bar_image = search_bar_image.resize((300, 130))
+search_bar_photo = ImageTk.PhotoImage(resized_search_bar_image)
+Label(root, image=search_bar_photo, bg="#47ceff", highlightthickness=0).place(x=235, y=34)
+
+# Search icon image
+search_icon_image = Image.open("App icon resources/Search icon.png")
+resized_search_icon_image = search_icon_image.resize((35,35))
+search_icon_photo = ImageTk.PhotoImage(resized_search_icon_image)
+Button(root, image=search_icon_photo, command=search, bg="#000000", bd=0, highlightthickness=0
+).place(x=480, y=82)
+
 
 
 
