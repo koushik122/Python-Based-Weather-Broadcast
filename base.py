36d76f59b099a77f3a0dd4479b5562372c646c4b
Sayan_Sen09 <sensayan1001@gmail.com>
2024-05-08 23:37:23 +0530

    Added weather details

--- base.py
+++ base.py
@@ -35,6 +35,50 @@ button = ctk.CTkButton(root, image=search_icon_image,text="",
                          command=search)
 button.place(x=570,y=60)
 
+#Weather Detail 
+canvas=Canvas(root,bg="black",highlightbackground="black",height=180,width=200)
+canvas.pack(side='left',padx=10)
+
+label1 = ctk.CTkLabel(canvas, text="Humidity:", fg_color="black", text_color="white")
+label1.place(x=10,y=20)
+label2 = ctk.CTkLabel(canvas, text="Wind Speed:", fg_color="black", text_color="white")
+label2.place(x=10,y=50)
+label3 = ctk.CTkLabel(canvas, text="AQI:", fg_color="black", text_color="white")
+label3.place(x=10,y=80)
+label4 = ctk.CTkLabel(canvas, text="Pressure:", fg_color="black", text_color="white")
+label4.place(x=10,y=110)
+label5 = ctk.CTkLabel(canvas, text="Visibility:", fg_color="black", text_color="white")
+label5.place(x=10,y=140)
+
+Humidity_variable = ctk.StringVar()
+Wind_Speed_variable = ctk.StringVar()
+AQI_variable = ctk.StringVar()
+Pressure_variable = ctk.StringVar()
+Visibility_variable = ctk.StringVar()
+
+
+label1 = ctk.CTkLabel(canvas, textvariable=Humidity_variable, fg_color="black", text_color="white")
+label1.place(x=90,y=20)
+
+label1 = ctk.CTkLabel(canvas, textvariable=Wind_Speed_variable, fg_color="black", text_color="white")
+label1.place(x=90,y=50)
+
+label1 = ctk.CTkLabel(canvas, textvariable=AQI_variable, fg_color="black", text_color="white")
+label1.place(x=90,y=80)
+
+label1 = ctk.CTkLabel(canvas, textvariable=Pressure_variable, fg_color="black", text_color="white")
+label1.place(x=90,y=110)
+
+label1 = ctk.CTkLabel(canvas, textvariable=Visibility_variable, fg_color="black", text_color="white")
+label1.place(x=90,y=140)
+
+
+
+
+
+#icon label
+label1 = ctk.CTkLabel(root, text="Humidity:", fg_color="black", text_color="white")
+label1.place(x=500,y=200)
 
 
 
