7959ad2ae5a2c074c8dc1d542fd7826b7ca1c50d
koushik122 <koushikpaul378@gmail.com>
2024-05-16 20:13:37 +0530

    Added weather forecast and weekday button for navigate, changed window size

--- base.py
+++ base.py
@@ -10,28 +10,32 @@ import cred
 
 
 obj = TimezoneFinder()
+
 def search():
   city=entry.get()
   url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={cred.api_key}"
-  res=requests.get(url)
-  response=res.json()
-  if res.status_code>=200 and res.status_code<=299:
-    icon= (response['weather'][0]['icon'])
+  api_response=requests.get(url)
+  global local_now
+  if api_response.status_code>=200 and api_response.status_code<=299:
+    response=api_response.json()
+    longitude=response['coord']['lon']
+    latitude=response['coord']['lat']
+    timezone_str=obj.timezone_at(lng=longitude, lat=latitude)
+    utc_now = datetime.datetime.now()
+    timezone = pytz.timezone(timezone_str)
+    local_now = utc_now.astimezone(timezone)
+
+    icon = (response['weather'][0]['icon'])
     icon_name=f"{icon}@2x.png"
     icon_image=ctk.CTkImage(light_image=Image.open(f"Weather Condition Icons/{icon_name}"),size=(150, 150))
     icon_lable.configure(image=icon_image)
 
-    lng=response['coord']['lon']
-    lat=response['coord']['lat']
-    timezone=obj.timezone_at(lng=lng, lat=lat)
-    utc_now = datetime.datetime.now()
-    timezone = pytz.timezone(timezone)
-    local_now = utc_now.astimezone(timezone)
     time = local_now.strftime("%I:%M %p")
     date = local_now.strftime("%a, %d %b")
     
     time_lable.configure(text=time)
     date_lable.configure(text=date)
+    timezone_variable.set(timezone_str)
 
     temp=f"{str(round(float(response['main']['temp'])))}°C"
     tempareture.set(temp)
@@ -42,8 +46,8 @@ def search():
     description = (response['weather'][0]['description'])
     icon_lable_variable.set(description.title())
 
-# Weather deta call
-    aqi_url=f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lng}&lon={lat}&appid={cred.api_key}"
+# AQI API call
+    aqi_url=f"http://api.openweathermap.org/data/2.5/air_pollution?lat={latitude}&lon={longitude}&appid={cred.api_key}"
     aqi_response=requests.get(aqi_url).json()
     humi= f"{(response['main']['humidity'])}%"
     wind= f"{(response['wind']['speed'])} m/s"
@@ -70,45 +74,14 @@ def search():
       case _:
         AQI_variable.set("No value")  
 
+    weekday_button_name_change()
+    forecast_api_call()
+    today()
 
   else:
     tkinter.messagebox.showwarning("No Result",  "-Enter valid city name\n-Check your internet connection")
 
-def on_click(event):
-  search()
-
-root =ctk.CTk()
-
-# Geometry (Width x Height)
-root.geometry("760x500")
-root.title("Weather App")
-
-# Disallow resizing
-root.resizable(width=False, height=False)
-
-# Background colour
-root.config(bg="#47ceff")
-
-
-# Search Bar Entry
-entry = ctk.CTkEntry(root,width=366,height=55, corner_radius=30, 
-                     placeholder_text="Enter City Name", font=('Arial', 27),
-                       bg_color="#47ceff", fg_color="#000",text_color='#fff', justify=CENTER)
-entry.place(x=200,y=60)
-entry.bind('<Return>', on_click)
-
-# Search icon image
-search_icon_image = ctk.CTkImage(dark_image=Image.open("App icon resources/Search_icon.png"),size=(35, 35))
-button = ctk.CTkButton(root, image=search_icon_image,text="",
-                       width=33,corner_radius=90,height=55,
-                        fg_color="#000",bg_color="#47ceff",
-                         command=search)
-button.place(x=570,y=60)
-
-# Weather Detail  
-canvas=Canvas(root,bg="#47ceff",highlightbackground="#47ceff",height=210,width=200)
-canvas.place(x=10,y=200)
-
+# To make rounded corner canvas
 def round_rectangle(x1, y1, x2, y2, radius=25, **kwargs):
         
     points = [x1+radius, y1,
@@ -134,8 +107,153 @@ def round_rectangle(x1, y1, x2, y2, radius=25, **kwargs):
 
     return canvas.create_polygon(points, **kwargs, smooth=True)
 
-my_rectangle = round_rectangle(4, 4, 200, 210, radius=20, fill="black")
+def on_press(event):
+  search()
 
+def forecast_api_call():
+  global forecast_response
+  city=entry.get()
+  forecast_url=f"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={cred.api_key}"
+  forecast_api_response=requests.get(forecast_url)
+  if forecast_api_response.status_code>=200 and forecast_api_response.status_code<=299:
+    forecast_response=forecast_api_response.json()
+
+  else:
+    tkinter.messagebox.showwarning("Failed To Load Forecast Data",  "-Enter valid city name\n-Check your internet connection")
+
+
+def weekday_button_name_change():
+  global today_date
+  global first_date
+  global second_date
+  global third_date
+  global forth_date
+  global fifth_date
+  today = local_now.today()
+
+  first_day_datetime = today + datetime.timedelta(days=1)
+  second_day_datetime = today + datetime.timedelta(days=2)
+  third_day_datetime = today + datetime.timedelta(days=3)
+  forth_day_datetime = today + datetime.timedelta(days=4)
+  fifth_day_datetime = today + datetime.timedelta(days=5)
+
+  # Weekday button name change
+  day_1_button.configure(text=first_day_datetime.strftime("%A"))
+  day_2_button.configure(text=second_day_datetime.strftime("%A"))
+  day_3_button.configure(text=third_day_datetime.strftime("%A"))
+  day_4_button.configure(text=forth_day_datetime.strftime("%A"))
+  day_5_button.configure(text=fifth_day_datetime.strftime("%A"))
+
+  # Next 5 days date including today
+  today_date=local_now.strftime("%Y-%m-%d")
+  first_date=first_day_datetime.strftime("%Y-%m-%d")
+  second_date=second_day_datetime.strftime("%Y-%m-%d")
+  third_date=third_day_datetime.strftime("%Y-%m-%d")
+  forth_date=forth_day_datetime.strftime("%Y-%m-%d")
+  fifth_date=fifth_day_datetime.strftime("%Y-%m-%d")
+
+def convert_time_to_12hr_format(time_str):
+  time_obj = datetime.datetime.strptime(time_str, "%H:%M:%S")
+  return time_obj.strftime("%I %p").lstrip('0')
+
+def make_rectangle(weather_count):
+  left, top, rignt, bottom = 20,20,150,180
+  for i in range(weather_count):
+    Canvas.create_rectangle((left, top, rignt, bottom), fill='green')
+    left+=150
+    rignt+=150
+
+def clear(Canvas):
+  Canvas.delete("all")
+  for label in labels:
+    label.destroy()
+  labels.clear()
+
+def today():
+  clear(Canvas)
+  target_date=today_date
+  weather_updates_count = 0
+  
+  x_value, y_value = 20,20
+  for item in forecast_response['list']:
+    date = item['dt_txt'].split(' ')[0]
+    if date == target_date:
+        weather_updates_count += 1
+        time = item['dt_txt'].split(' ')[1]
+        weather_f_des_str=(item['weather'][0]['description'])
+        tempareture_str=f"{str(round(float(item['main']['temp'])))}°C"
+        time_str=convert_time_to_12hr_format(time)
+        combi_text=f"{(weather_f_des_str.title())}\n{tempareture_str}\n\n{time_str}"
+        icon = (item['weather'][0]['icon'])
+        icon_name=f"{icon}@2x.png"
+        icon_image=ctk.CTkImage(light_image=Image.open(f"Weather Condition Icons/{icon_name}"),size=(50,50))
+        forecast_lables=ctk.CTkLabel(Canvas,width=100,image=icon_image,text=combi_text,bg_color="green",compound="top")
+        forecast_lables.place(x = x_value,y = y_value)
+        labels.append(forecast_lables)
+        x_value+=120
+
+  make_rectangle(weather_updates_count)
+ 
+
+def day_1():
+  clear(Canvas)
+  target_date=first_date
+
+
+def day_2():
+  clear(Canvas)
+  target_date=second_date
+
+
+def day_3():
+  clear(Canvas)
+  target_date=third_date
+
+
+def day_4():
+  clear(Canvas)
+  target_date=forth_date
+
+
+def day_5():
+  clear(Canvas)
+  target_date=fifth_date
+
+
+root = ctk.CTk()
+
+# Geometry (Width x Height)
+root.geometry("980x550")
+root.title("Weather App")
+
+# Disallow resizing
+root.resizable(width=False, height=False)
+
+# Background colour
+root.config(bg="#47ceff")
+
+
+# Search Bar Entry
+entry = ctk.CTkEntry(root,width=366,height=55, corner_radius=30, 
+                     placeholder_text="Enter City Name", font=('Arial', 27),
+                       bg_color="#47ceff", fg_color="#000",text_color='#fff', justify=CENTER)
+entry.place(x=200,y=60)
+entry.bind('<Return>', on_press)
+
+# Search icon image
+search_icon_image = ctk.CTkImage(dark_image=Image.open("App icon resources/Search_icon.png"),size=(35, 35))
+button = ctk.CTkButton(root, image=search_icon_image,text="",
+                       width=33,corner_radius=90,height=55,
+                        fg_color="#000",bg_color="#47ceff",
+                         command=search)
+button.place(x=570,y=60)
+
+# Weather Detail 
+canvas=Canvas(root,bg="#47ceff",highlightbackground="#47ceff",height=210,width=200)
+canvas.place(x=10,y=200)
+
+# Created rounded rectangle
+my_rectangle = round_rectangle(4, 4, 200, 210, radius=20, fill="black")
 
 # Weather details lables
 label1 = ctk.CTkLabel(canvas, text="Humidity:", fg_color="black",bg_color="black", text_color="white")
@@ -171,9 +289,6 @@ t_label4.place(x=90,y=102)
 t_label5 = ctk.CTkLabel(canvas, textvariable=Visibility_variable, fg_color="black",bg_color="black", text_color="white")
 t_label5.place(x=90,y=132)
 
-t_label1 = ctk.CTkLabel(canvas, textvariable=Humidity_variable, fg_color="black",bg_color="black", text_color="white")
-t_label1.place(x=90,y=12)
-
 
 # Tempareture lable
 tempareture = ctk.StringVar()
@@ -186,7 +301,7 @@ fl_temp_lable.place(x=300,y=250)
 
 
 # Icon label
-icon_lable = ctk.CTkLabel(root,text="", bg_color='#47ceff')
+icon_lable = ctk.CTkLabel(root,text="",bg_color='#47ceff')
 icon_lable.place(x=500,y=160)
 
 # Icon lable description
@@ -201,5 +316,42 @@ time_lable.place(x=5,y=3)
 
 date_lable = ctk.CTkLabel(root,text="",text_color='#262e94', bg_color='#47ceff', font=('Arial', 25))
 date_lable.place(x=5,y=28)
+
+# Timezone lable
+timezone_variable = ctk.StringVar()
+timezone_lable = ctk.CTkLabel(root, textvariable=timezone_variable, text_color='#262e94', bg_color='#47ceff', font=('Arial', 25))
+timezone_lable.place(x=800,y=10)
+
+
+# Canvas for weather forecast
+# scrollregion = (1eft, top, right, bottom) 
+Canvas = Canvas(root,height=200, bg="black")
+Canvas.pack(expand=True, anchor='sw', fill=X)
+
+# To store labels for forecast
+labels=[]
+
+
+
+today_button=ctk.CTkButton(root,width=80, text="Today",bg_color="#47ceff",corner_radius=5, font=('Arial', 16), command = today)
+today_button.place(x=50,y=350)
+
+day_1_button=ctk.CTkButton(root,width=100, text="Day 1",bg_color="#47ceff",corner_radius=5, font=('Arial', 16),command = day_1)
+day_1_button.place(x=180,y=350)
+
+day_2_button=ctk.CTkButton(root,width=100, text="Day 2",bg_color="#47ceff",corner_radius=5, font=('Arial', 16),command = day_2)
+day_2_button.place(x=340,y=350)
+
+day_3_button=ctk.CTkButton(root, width = 100, text = "Day 3",bg_color="#47ceff",corner_radius=5, font=('Arial', 16),command = day_3)
+day_3_button.place(x=500,y=350)
+
+day_4_button=ctk.CTkButton(root,width=100, text="Day 4",bg_color="#47ceff",corner_radius=5, font=('Arial', 16),command=day_4)
+day_4_button.place(x=660,y=350)
+
+day_5_button=ctk.CTkButton(root,width=100, text="Day 5",bg_color="#47ceff",corner_radius=5, font=('Arial', 16),command=day_5)
+day_5_button.place(x=820,y=350)
+
+
+
 
 root.mainloop()
